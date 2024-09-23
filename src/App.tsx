3642b5326281853a65ab17e225c9e4434f969419
DangHuynh <dang.huynh@samsung.comh>
2024-09-23 12:53:43 +0700

    first deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,63 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { io } from "socket.io-client";
-import Peer from "peerjs";
+import Peer, { MediaConnection } from "peerjs";
 import "./App.css";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://chat-dan-server.onrender.com");
+
 
 function App() {
   const myVideo = useRef<HTMLVideoElement>(null);
   const friendVideo = useRef<HTMLVideoElement>(null);
-  const [stream, setStream] = useState();
   const peer = new Peer();
-
-  // console.log(123321, peer);
-
-  const [id, setId] = useState<string>("");
+  const peers: { [friendId: string]: MediaConnection; } = {};
 
   useEffect(() => {
-    socket.on("user-id", (userId: string) => setId(userId));
-
-    // peer.on("signal", () => {
-    //   socket.emit("join", id);
-    // });
-
-    // peer.on("stream", (stream) => {
-    //   friendVideo.current!.srcObject = stream;
-    //   console.log(123321);
-    // });
-
-    socket.on("new-user", (newUser) => console.log("New user : ", newUser));
+    peer.on("open", peerId => socket.emit("join", peerId));
 
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: true })
       .then((stream) => {
         myVideo.current!.srcObject = stream;
+
+        peer.on("call", call => {
+          console.log("Someone is calling u");
+
+          call.answer(stream);
+          call.on("stream", remoteStream => {
+            console.log("Calllllllllll");
+            friendVideo.current!.srcObject = remoteStream;
+          });
+
+          call.on('close', () => {
+            console.log("call close 222");
+
+          });
+        });
+
+        socket.on("new-user", (friendId) => {
+          console.log("Calling new friend: ", friendId);
+          const call = peer.call(friendId, stream);
+
+          console.log("Calling new friend 2");
+
+          call.on("stream", remoteStream => {
+            console.log("Calling new friend 3");
+            friendVideo.current!.srcObject = remoteStream;
+          });
+
+          call.on('close', () => {
+            console.log("call close");
+
+          });
+          peers[friendId] = call;
+        });
+
       });
   }, []);
 
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <video ref={myVideo} autoPlay></video>
       <video ref={friendVideo} autoPlay></video>
     </div>
