549091a8680733bb3cd050a1a4e557667baa9d8b
DangHuynh <dang.huynh@samsung.comh>
2024-10-03 18:33:31 +0700

    v241003.1

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -1,12 +1,12 @@
 interface ChatBubbleProps {
     text: string;
     end?: boolean;
-    toLeft?: boolean;
-    style?: string;
+    stlyeWrapper?: string;
+    styleBubble?: string;
 }
 
-export default function ChatBubble({ text, end, toLeft, style }: ChatBubbleProps) {
-    return <div className={`chat ${end ? 'chat-end' : 'chat-start'} ${toLeft ? 'place-items-start' : 'place-items-end'}`}>
-        <div className={`chat-bubble transition-all duration-200 bg-white text-black px-[16px] py-[10px] animate-jump animate-duration-1000 animate-ease-in ${style}`}>{text}</div>
+export default function ChatBubble({ text, end, stlyeWrapper, styleBubble }: ChatBubbleProps) {
+    return <div className={`chat ${end ? 'chat-end' : 'chat-start'} ${stlyeWrapper}`}>
+        <div className={`chat-bubble transition-all duration-200 bg-white text-black px-[16px] py-[10px] ${styleBubble}`}>{text}</div>
     </div>;
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,93 +3,73 @@ import { ChatBubble } from '../components';
 import { useEffect, useRef, useState } from 'react';
 
 export default function HomePage() {
-    const [leftMessages, setLeftMessages] = useState<string[]>(() => {
-        return [
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.'
-        ];
-    });
-    const [rightMessages, setRightMessages] = useState<string[]>(() => {
-        return [
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.',
-            'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo.'
-        ];
-    });
+    const [messages, setMessages] = useState<string[]>(() => [
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 1',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 2',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 3',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 4',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 5',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 6',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 7',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 8',
+        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 9'
+    ]);
+    const [yourMessage, setYourMessage] = useState<string>('');
     const messageViewRef = useRef<HTMLDivElement>(null);
+    const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
     const addMessage = (message: string) => {
-        if (leftMessages.length <= rightMessages.length) {
-            setLeftMessages([message, ...leftMessages]);
-        } else {
-            setRightMessages([message, ...rightMessages]);
-        }
+        setMessages([...messages, message]);
     };
 
     const scrollToBottom = () => {
-        setTimeout(() => {
-
-            messageViewRef.current?.scrollTo({ top: messageViewRef.current.scrollHeight, behavior: 'smooth' });
-        });
+        setTimeout(() => messageViewRef.current?.scrollTo({ top: messageViewRef.current.scrollHeight, behavior: 'smooth' }));
     };
 
-    useEffect(() => scrollToBottom(), []);
+    return (
+        <div className="w-full h-full grid place-items-center bg-[var(--primary)] relative">
 
-    return <div className="w-full h-full grid place-items-center bg-[var(--primary)] relative">
+            {/* Background */}
+            <div className='relative'>
+                <img src={earthLogo} alt="earth-logo" />
+                <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-white w-max'>Chat Dan</span>
+            </div>
 
-        {/* Background */}
-        <div className='relative'>
-            <img src={earthLogo} alt="earth-logo" />
-            <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-white w-max'>Chat Dan</span>
-        </div>
-
-        {/* Content */}
-        <div className='absolute inset-[20px] flex flex-col gap-[30px]'>
-            <div className='h-[calc(100%-40px)] relative'>
-                {/* Messages */}
-                <div ref={messageViewRef} className='h-full overflow-y-scroll'>
-                    <div className='flex gap-[280px] px-[20px] py-[80px]'>
-                        <div className='flex-1 flex flex-col-reverse gap-[40px]'>
-                            {leftMessages.map((message, index) => {
-                                if (index % 2 === 0) {
-                                    return (<ChatBubble key={index} end style='max-w-[60%]' text={message} />);
-                                }
-                                return (<ChatBubble key={index} end toLeft style='max-w-[60%]' text={message} />);
-                            })}
-                        </div>
-                        <div className='flex-1 flex flex-col-reverse gap-[40px] pb-[40px]'>
-                            {rightMessages.map((message, index) => {
-                                if (index % 2 === 0) {
-                                    return (<ChatBubble key={index} toLeft style='max-w-[60%]' text={message} />);
-                                }
-                                return (<ChatBubble key={index} style='max-w-[60%]' text={message} />);
-                            })}
-                        </div>
+            {/* Content */}
+            <div className='absolute inset-[10px] bottom-[70px] flex flex-col items-center gap-[20px]'>
+                <div className='h-[calc(100%-40px)] w-full relative flex flex-col justify-end'>
+                    {/* Messages */}
+                    <div ref={messageViewRef} className='overflow-y-scroll p-[40px]'>
+                        {messages.map((message, index) => {
+                            if (index % 2 === 0) {
+                                return (<ChatBubble key={index} text={message} end stlyeWrapper='place-items-start' styleBubble='max-w-[40%]' />);
+                            }
+                            return (<ChatBubble key={index} text={message} stlyeWrapper='place-items-end' styleBubble='max-w-[40%]' />);
+                        })}
                     </div>
-                </div>
 
-                {/* Top and bottom blur layers */}
-                <div className='absolute top-0 left-0 w-full h-[80px] bg-gradient-to-b from-[var(--primary)] to-transparent'></div>
-                <div className='absolute bottom-0 left-0 w-full h-[80px] bg-gradient-to-t from-[var(--primary)] to-transparent'></div>
+                    {/* Top and bottom blur layers */}
+                    <div className='absolute top-0 left-0 w-full h-[40px] bg-gradient-to-b from-[var(--primary)] to-transparent'></div>
+                    <div className='absolute bottom-0 left-0 w-full h-[40px] bg-gradient-to-t from-[var(--primary)] to-transparent'></div>
+                </div>
             </div>
 
             {/* Typing */}
-            <div className='h-[40px] bg-white'
-                onClick={() => {
-                    addMessage('anabsdgasj 1616161717717171' + Math.random());
-
+            <div className='absolute bottom-[70px] w-[90%] max-w-[600px] flex gap-[15px] items-end'>
+                <textarea ref={textAreaRef} className='flex-1 px-[10px] py-[5px] outline-none rounded-[24px] shadow-inner shadow-slate-400' placeholder='Type your message here'
+                    onChange={e => {
+                        setYourMessage(e.target.value);
+                        if (textAreaRef.current) {
+                            textAreaRef.current.style.height = 'unset';
+                            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
+                        }
+                    }}
+                ></textarea>
+                <button className='bg-[var(--secondary)] px-[20px] rounded-[24px] hover:scale-110 transition-all text-white font-bold' onClick={() => {
+                    addMessage('Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. ' + Math.random());
                     scrollToBottom();
-                }}
-            >Click</div>
+                }}>Send</button>
+            </div>
         </div>
-    </div>;
+    );
 }
