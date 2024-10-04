1fee04c068849e644af21d26a5bbb75bda01651e
DangHuynh <dang.huynh@samsung.comh>
2024-10-04 17:22:52 +0700

    v241004.1

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -7,6 +7,6 @@ interface ChatBubbleProps {
 
 export default function ChatBubble({ text, end, stlyeWrapper, styleBubble }: ChatBubbleProps) {
     return <div className={`chat ${end ? 'chat-end' : 'chat-start'} ${stlyeWrapper}`}>
-        <div className={`chat-bubble transition-all duration-200 bg-white text-black px-[16px] py-[10px] ${styleBubble}`}>{text}</div>
+        <div className={`chat-bubble transition-all duration-200 bg-white text-black px-[16px] py-[10px] text-wrap drop-shadow-md break-words ${styleBubble}`}>{text}</div>
     </div>;
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,75 +1,86 @@
 import earthLogo from '/earth.svg';
 import { ChatBubble } from '../components';
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 
 export default function HomePage() {
     const [messages, setMessages] = useState<string[]>(() => [
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 1',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 2',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 3',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 4',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 5',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 6',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 7',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 8',
-        // 'Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. 9'
+        'Hi, welcome to Chat Dan',
+        'You can chat with everyone here anonymously'
     ]);
     const [yourMessage, setYourMessage] = useState<string>('');
     const messageViewRef = useRef<HTMLDivElement>(null);
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
-    const addMessage = (message: string) => {
-        setMessages([...messages, message]);
+    const updateYourMessage = (message: string) => {
+        setYourMessage(message);
+        resizeTextAreaHeight();
+    };
+
+    const sendMessage = (message: string) => {
+        if (message.trim() !== '') {
+            setMessages([...messages, message]);
+            setYourMessage('');
+            textAreaRef.current!.value = '';
+            resizeTextAreaHeight();
+            scrollToBottom();
+            textAreaRef.current!.focus();
+        }
     };
 
     const scrollToBottom = () => {
         setTimeout(() => messageViewRef.current?.scrollTo({ top: messageViewRef.current.scrollHeight, behavior: 'smooth' }));
     };
 
+    const resizeTextAreaHeight = () => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = 'unset';
+            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
+        }
+    };
+
     return (
         <div className="w-full h-full grid place-items-center bg-[var(--primary)] relative">
 
             {/* Background */}
-            <div className='relative'>
-                <img src={earthLogo} alt="earth-logo" />
-                <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-white w-max'>Chat Dan</span>
+            <div className='relative w-[80%] max-w-[450px]'>
+                <img className='animate-spin animate-infinite animate-duration-[70s]' src={earthLogo} alt="earth-logo" />
+                <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-white w-max opacity-80 text-5xl md:text-6xl md:opacity-100'>Chat Dan</span>
             </div>
 
             {/* Content */}
-            <div className='absolute inset-[10px] bottom-[70px] flex flex-col items-center gap-[20px]'>
-                <div className='h-[calc(100%-40px)] w-full relative flex flex-col justify-end'>
-                    {/* Messages */}
-                    <div ref={messageViewRef} className='overflow-y-scroll p-[40px]'>
-                        {messages.map((message, index) => {
-                            if (index % 2 === 0) {
-                                return (<ChatBubble key={index} text={message} end stlyeWrapper='place-items-start' styleBubble='max-w-[40%]' />);
-                            }
-                            return (<ChatBubble key={index} text={message} stlyeWrapper='place-items-end' styleBubble='max-w-[40%]' />);
-                        })}
-                    </div>
+            <div className='absolute inset-x-0 inset-y-[10px] flex flex-col justify-end items-center gap-[30px]'>
+                {/* Messages */}
+                <div ref={messageViewRef} className='w-full overflow-y-scroll px-[10px] flex flex-col gap-[20px] md:px-[30px]'>
+                    {messages.map((message, index) => {
+                        if (index % 2 === 0) {
+                            return (<ChatBubble key={index} text={message} stlyeWrapper='place-items-start' styleBubble='md:max-w-[30%]' />);
+                        }
+                        return (<ChatBubble key={index} text={message} end stlyeWrapper='place-items-end' styleBubble='md:max-w-[30%]' />);
+                    })}
+                </div>
 
-                    {/* Top and bottom blur layers */}
-                    <div className='absolute top-0 left-0 w-full h-[40px] bg-gradient-to-b from-[var(--primary)] to-transparent'></div>
-                    <div className='absolute bottom-0 left-0 w-full h-[40px] bg-gradient-to-t from-[var(--primary)] to-transparent'></div>
+                {/* Typing */}
+                <div className='w-[90%] flex flex-col gap-[5px]'>
+                    <textarea ref={textAreaRef} rows={1}
+                        className='w-full p-[8px] outline-none rounded-[24px] shadow-inner shadow-slate-400 resize-none placeholder:text-center max-h-[18vh]' placeholder='Type your message here'
+                        onChange={e => updateYourMessage(e.target.value)}
+                        onKeyDown={e => {
+                            scrollToBottom();
+                            if (e.key === 'Enter' && !e.shiftKey) {
+                                e.preventDefault();
+                                sendMessage(yourMessage);
+                            }
+                        }}
+                    ></textarea>
+                    <button
+                        disabled={yourMessage.trim() === '' ? true : false}
+                        className={`w-full bg-[var(--secondary)] px-[20px] rounded-[24px] disabled:bg-gray-500 enabled:hover:scale-110 transition-all text-white font-bold h-[35px]`}
+                        onClick={() => sendMessage(yourMessage)}
+                    >
+                        Send
+                    </button>
                 </div>
             </div>
-
-            {/* Typing */}
-            <div className='absolute bottom-[70px] w-[90%] max-w-[600px] flex gap-[15px] items-end'>
-                <textarea ref={textAreaRef} className='flex-1 px-[10px] py-[5px] outline-none rounded-[24px] shadow-inner shadow-slate-400' placeholder='Type your message here'
-                    onChange={e => {
-                        setYourMessage(e.target.value);
-                        if (textAreaRef.current) {
-                            textAreaRef.current.style.height = 'unset';
-                            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
-                        }
-                    }}
-                ></textarea>
-                <button className='bg-[var(--secondary)] px-[20px] rounded-[24px] hover:scale-110 transition-all text-white font-bold' onClick={() => {
-                    addMessage('Lorem sibum kimchana mala bimula saya! Wohaa na miya loo dpan sarandeyo. ' + Math.random());
-                    scrollToBottom();
-                }}>Send</button>
-            </div>
-        </div>
+        </div >
     );
 }
