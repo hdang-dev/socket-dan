f145ca7e20e10463e401312fc062a36fc9c3c11c
hhddang <dangtramthien@gmail.com>
2024-10-06 23:18:56 +0700

    v241006.1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,6 +9,7 @@ export default function HomePage() {
   const messageViewRef = useRef<HTMLDivElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const [userId, setUserId] = useState<string | null>(null);
+  const [showRoomButton, setShowRoomButton] = useState(false);
 
   const updateYourMessage = (message: string) => {
     setYourMessage(message);
@@ -62,6 +63,9 @@ export default function HomePage() {
     console.log(userId);
     emitEvent("JOIN_APP");
     onEvent("USER_ID", (userId) => setUserId(userId));
+  }, []);
+
+  useEffect(() => {
     onEvent("RECEIVE_MESSAGE", (message) => receiveMessage(message));
   }, [receiveMessage]);
 
@@ -73,10 +77,24 @@ export default function HomePage() {
         <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-white w-max text-5xl md:text-6xl">Chat Dan</span>
       </div>
 
+      {/* Options */}
+      <div
+        className={`absolute inset-0 transition-all duration-300 grid place-items-center z-[1] ${showRoomButton ? "backdrop-blur-md translate-y-0" : "translate-y-[calc(-100%+20px)]"}`}
+        onClick={() => setShowRoomButton(!showRoomButton)}>
+        <button className={`absolute w-[100px] h-[30px] bg-white shadow-md rounded-[24px] ${showRoomButton ? "bottom-full opacity-0" : "bottom-0 opacity-100"}`}></button>
+        <div onClick={(e) => e.stopPropagation()}>
+          <div className="w-[90%] max-w-[600px] rounded-[24px] shadow-sm">
+            <div className="flex">
+              <button className="border-[3px] border-[var(--secondary)] transition-all text-[var(--secondary)] py-[5px] px-[10px] rounded-[24px] bg-white font-bold">Room+</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Content */}
-      <div className="absolute inset-[10px] md:inset-x-[30px] md:bottom-[30px] flex flex-col justify-end items-center gap-[25px] md:gap-[40px]">
+      <div className="absolute inset-[10px] top-0 md:inset-x-[30px] md:bottom-[30px] flex flex-col justify-end items-center gap-[25px] md:gap-[40px]">
         {/* Messages */}
-        <div ref={messageViewRef} className="w-full overflow-y-auto flex flex-col gap-[20px] scrollbar-none">
+        <div ref={messageViewRef} className="pt-[30px] w-full overflow-y-auto flex flex-col gap-[20px] scrollbar-none">
           {messages.map((message, index) => {
             if (index % 2 === 0) {
               return <ChatBubble key={index} text={message} styleWrapper="place-items-start" styleBubble="md:max-w-[30%]" />;
