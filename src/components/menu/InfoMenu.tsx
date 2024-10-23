c023cbee2afe6667cd91a30c92919ad565e95003
DangHuynh <dang.huynh@samsung.comh>
2024-10-23 15:51:25 +0700

    v241023.4

--- src/components/menu/InfoMenu.tsx
+++ src/components/menu/InfoMenu.tsx
@@ -6,12 +6,16 @@ interface InfoMenuProps {
 }
 
 export function InfoMenu({ order, roomLink }: InfoMenuProps) {
+  const copyLink = () => {
+    navigator.clipboard.writeText(roomLink);
+  };
+
   return (
     <Section order={order}>
       <SubTitle text="Room Link" />
       <div className="flex gap-[10px] md:gap-[20px] flex-col md:flex-row items-center">
-        <span className="text-center md:text-start py-[5px] min-w-[300px] italic">{roomLink}</span>
-        <Button>Copy</Button>
+        <span className="text-center md:text-start italic py-[5px] min-w-[300px] max-w-full whitespace-nowrap overflow-scroll scrollbar-none">{roomLink}</span>
+        <Button onClick={() => copyLink()}>Copy</Button>
       </div>
 
       <SubTitle text="Members" />
