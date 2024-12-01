85e5427b8371a4deb552786befdc6f3274cbcdef
hhddang <dangtramthien@gmail.com>
2024-12-01 22:26:58 +0700

    v241201.1

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,41 +1,50 @@
 import { io } from "socket.io-client";
+import { User } from "../interfaces";
 
 const socket = io(import.meta.env.VITE_SERVER_URL);
 
 enum SocketEvent {
-  JOIN_APP = "JOIN_APP",
-  USER_DISCONNECT = "USER_DISCONNECT",
-  JOIN_ROOM = "JOIN_ROOM",
-  LEAVE_ROOM = "LEAVE_ROOM",
-  DATA = "DATA",
+  CONNECT = "SK_CONNECT",
+  GET_USERS = "SK_GET_USERS",
+  ADD_USER = "SK_ADD_USER",
+  REMOVE_USER = "SK_REMOVE_USER",
+  JOIN_ROOM = "SK_JOIN_ROOM",
+  LEAVE_ROOM = "SK_LEAVE_ROOM",
 }
 
-export const socketService = {
-  joinApp(handler: (userId: string) => void) {
-    socket.emit(SocketEvent.JOIN_APP);
-    socket.on(SocketEvent.JOIN_APP, handler);
+const socketService = {
+  connect(userName?: string, handler: (userId: string) => void) {
+    socket.emit(SocketEvent.CONNECT, userName);
+    socket.on(SocketEvent.CONNECT, handler);
   },
 
+  joinRoom(roomType: string, roomId: string, user: User, handler: (status: boolean) => void) {
+    socket.emit(SocketEvent.JOIN_ROOM, roomType, roomId);
+    socket.on(SocketEvent.JOIN_ROOM, handler);
+  },
+
+  leaveRoom(roomId: string) {
+    socket.emit(SocketEvent.LEAVE_ROOM, roomId);
+  },
+
+  getUSers(handler: (userId: string) => void) {
+
+  }
   userDisconnect(handler: (userId: string) => void) {
     socket.on(SocketEvent.USER_DISCONNECT, handler);
   },
 
-  joinRoom(roomId: string) {
-    socket.emit(SocketEvent.JOIN_ROOM, roomId);
-  },
 
-  leaveRoom() {
-    socket.emit(SocketEvent.LEAVE_ROOM);
-  },
+  // sendData<T>(id: string, key: string, data: T) {
+  //   socket.emit(SocketEvent.DATA, id, key, data);
+  // },
 
-  sendData<T>(id: string, key: string, data: T) {
-    socket.emit(SocketEvent.DATA, id, key, data);
-  },
-
-  receiveData<T>(key: string, handler: (data: T) => void) {
-    socket.on(SocketEvent.DATA, (onKey, data) => {
-      if (onKey === key) handler(data);
-    });
-    socket.
-  },
+  // receiveData<T>(key: string, handler: (data: T) => void) {
+  //   socket.on(SocketEvent.DATA, (onKey, data) => {
+  //     if (onKey === key) handler(data);
+  //   });
+  //   socket.
+  // },
 };
+
+export { socketService as socket };
